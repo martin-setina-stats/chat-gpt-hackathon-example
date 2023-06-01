04d5756d2c5b095c28e9ea4e4a23e58143ec7404
martin-setina-stats <103524332+martin-setina-stats@users.noreply.github.com>
2023-06-01 08:33:44 +0200

    feat: add a sample game

--- src/documentLoader.ts
+++ src/documentLoader.ts
@@ -2,7 +2,7 @@ import {S3Loader} from 'langchain/document_loaders/web/s3';
 
 const loader = new S3Loader({
   bucket: 'as-tools-hackaton',
-  key: 'test.txt', // Note - the folder 887fp0ih4vl22bqcj23x73klw/ and its contents didn't have correct permissions, so uploaded a file myself
+  key: 'test.json', // Note - the folder 887fp0ih4vl22bqcj23x73klw/ and its contents didn't have correct permissions, so uploaded a file myself
   unstructuredAPIURL: 'http://localhost:8000/general/v0/general'
 });
 
